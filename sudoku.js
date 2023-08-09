55a85309b79c17e2f72ae966c36ae9048b39f938
thilanka <thilanka.geshan@gmail.com>
2023-08-09 12:36:59 +0530

    setup selct number bar

---
+++ sudoku.js
@@ -0,0 +1,63 @@
+var numSelected = null;
+var tileSelected = null;
+
+var errors = 0;
+
+var board = [
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---"
+]
+
+var solution = [
+    "387491625",
+    "241568379",
+    "569327418",
+    "758619234",
+    "123784596",
+    "496253187",
+    "934176852",
+    "675832941",
+    "812945763"
+]
+
+window.onload = function(){
+    setGame();
+}
+
+function setGame() {
+    //digits 1-9
+    for (let i = 1; i<=9; i++) {
+        //<div id="1" class="number">1</div>
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerText = i;
+        number.addEventListener("click", selectNumber);
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    //board 9*9
+    for(let r = 0; r < 9; r++){
+        for(let c=0; c< 9; c++){
+            let title = document.createElement("div");
+            title.id = r.toString() + "-" + c.toString();
+            title.classList.add("title");
+            document.getElementById("board").append(title);
+        }
+    }
+}
+
+function selectNumber(){
+    if (numSelected != null) {
+        numSelected.classList.remove("number-selected");
+    }
+    numSelected = this;
+    numSelected.classList.add("number-selected");
+}
